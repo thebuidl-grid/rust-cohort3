b72ffb460a89573302735edf00a1e8128cd72595
macnelson9 <michaelofatu@gmail.com>
2026-02-20 12:30:21 +0100

    refactor: remove unused files

--- uche_calc/target/debug/Soma_cli_calc.d
+++
@@ -1,1 +0,0 @@
-/home/uche-ofatu/Desktop/rust-cohort3/uche_calc/target/debug/Soma_cli_calc: /home/uche-ofatu/Desktop/rust-cohort3/uche_calc/src/main.rs
--- uche_calc/target/debug/deps/Soma_cli_calc-0d35655a3a3530f8.d
+++
@@ -1,5 +0,0 @@
-/home/uche-ofatu/Desktop/rust-cohort3/uche_calc/target/debug/deps/Soma_cli_calc-0d35655a3a3530f8.d: src/main.rs
-
-/home/uche-ofatu/Desktop/rust-cohort3/uche_calc/target/debug/deps/Soma_cli_calc-0d35655a3a3530f8: src/main.rs
-
-src/main.rs:
