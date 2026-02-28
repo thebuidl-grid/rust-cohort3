7d6facdfb06a49acec41bb13f944c546aa2734b4
manuelusman73-png <manuelusman73@gmail.com>
2026-02-28 13:19:17 +0000

    feat: cli_calculator

---
+++ usman_cli/target/debug/deps/usman_cli-de38a1a654065723.d
@@ -0,0 +1,5 @@
+/workspaces/rust-cohort3/usman_cli/target/debug/deps/usman_cli-de38a1a654065723.d: src/main.rs
+
+/workspaces/rust-cohort3/usman_cli/target/debug/deps/usman_cli-de38a1a654065723: src/main.rs
+
+src/main.rs:
---
+++ usman_cli/target/debug/usman_cli.d
@@ -0,0 +1,1 @@
+/workspaces/rust-cohort3/usman_cli/target/debug/usman_cli: /workspaces/rust-cohort3/usman_cli/src/main.rs
